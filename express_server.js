0637c42fdd45c2b25f9d7fd0ad9c1987ade7e2af
vagrant <r.ali@mail.utoronto.ca>
2019-05-10 18:02:54 +0000

    added new cookie middleware: cookie-session. instead of cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -32,7 +36,6 @@ function urlsForUser(id) {
     if (urlDatabase[shortUrl].userID === id) {
       urls[shortUrl] = {longURL: urlDatabase[shortUrl].longURL}
     }
-    console.log(urls)
   } return urls
 }
 
@@ -78,7 +81,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   if (userid) {
     let templateVars = { user: users[userid] };
     res.render("urls_new", templateVars);
@@ -88,7 +91,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   if (userid) {
     const userUrls = urlsForUser(userid)
     let templateVars = { urls: userUrls, user: users[userid]};
@@ -98,27 +101,27 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const tinyString = generateRandomString();
-  urlDatabase[tinyString] = {longURL: req.body.longURL, userID:req.cookies.user_id}
+  urlDatabase[tinyString] = {longURL: req.body.longURL, userID:req.session.user_id}
   res.redirect("/urls/"+tinyString);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (req.cookies.user_id === urlDatabase[req.params.shortURL].userID) {
+  if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
     res.redirect('/urls')
   } else res.send("You don't have permission")
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  if (req.cookies.user_id === urlDatabase[req.params.shortURL].userID) {
+  if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL
-  urlDatabase[tinyString].userID = req.cookies.user_id
+  urlDatabase[tinyString].userID = req.session.user_id
   res.redirect("/urls");
   } else res.send("You do not have permission")
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const userid = req.cookies.user_id
+  const userid = req.session.user_id
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[userid]}
   res.render("urls_show", templateVars);
 });
@@ -130,7 +133,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  let templateVars = { user: users[req.cookies.user_id]}
+  let templateVars = { user: users[req.session.user_id]}
   res.render("user_registration", templateVars);
 });
 
@@ -148,13 +151,13 @@ app.post("/register", (req, res) => {
       email: req.body.email,
       password: hashedPassword
     }
-    res.cookie("user_id", id);
+    req.session.user_id = users[id].id;
     res.redirect("/urls");
   }
 })
 
 app.get("/login", (req, res) => {
-  let templateVars = { user: users[req.cookies.user_id]}
+  let templateVars = { user: users[req.session]}
   res.render("user_login", templateVars)
 })
 
@@ -166,12 +169,12 @@ app.post("/login", (req, res) => {
   if (!(bcrypt.compareSync(req.body.password, users[uID].password))) {
     res.status(403).send("Incorrect Password")
   } else if (uID && (bcrypt.compareSync(req.body.password, users[uID].password))) {
-    res.cookie("user_id", users[uID].id)
+    req.session.user_id = users[uID].id
     res.redirect("/urls");
   }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/urls");
 });
